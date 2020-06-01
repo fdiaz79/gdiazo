4b174d5f90a3a1b97af19c7a41ea6e6e24b8bd57
Fabian Diaz <16745697+fdiaz79@users.noreply.github.com>
2020-05-31 23:04:46 -0400

    gallery-carousel styling

--- src/components/CarouselComp.js
+++ src/components/CarouselComp.js
@@ -8,24 +8,30 @@ const CarouselComp = () => {
     return (
         <div className='carousel-comp' >
             
-            <CarouselProvider naturalSlideHeight={30} naturalSlideWidth={30} totalSlides={4} visibleSlides={3}>
-                <Slider>
-                    <Slide index={0} className="gallery-picture">
-                        <Image src={'/images/raices-modified.png'} />
-                    </Slide>
-                    <Slide index={1} className="gallery-picture">
-                        <Image src={'/images/catleya-modified.png'} />
-                    </Slide>                        
-                    <Slide index={2} className="gallery-picture">
-                        <Image src={'/images/raices-modified.png'} />
-                    </Slide>                        
-                    <Slide index={3} className="gallery-picture">
-                        <Image src={'/images/catleya-modified.png'} />
-                    </Slide>               
-                </Slider>
-                <ButtonBack>Back</ButtonBack>
-                <ButtonNext>Next</ButtonNext>
-                <DotGroup disableActiveDots={false} />
+            <CarouselProvider naturalSlideHeight={30} naturalSlideWidth={30} totalSlides={4} visibleSlides={3} step={3}>
+                <div className='carousel-container'>
+                    <Slider>
+                        <Slide index={0} className="gallery-picture">                            
+                            {/* <img src='/images/raices-modified.png' alt='img-thumb' className='image___xtQGH carousel__image carousel__image--success' /> */}
+                            <Image src={'/images/raices-modified.png'} isBgImage={true} /> 
+                            <h3 className='gallery-name'>FIGURA HUMANA</h3>                            
+                        </Slide>
+                        <Slide index={1} className="gallery-picture">
+                            <Image src={'/images/catleya-modified.png'} />
+                            <h3 className='gallery-name'>ABSTRACTO</h3>
+                        </Slide>                        
+                        <Slide index={2} className="gallery-picture">
+                            <Image src={'/images/raices-modified.png'} />
+                        </Slide>                        
+                        <Slide index={3} className="gallery-picture">
+                            <Image src={'/images/catleya-modified.png'} />
+                        </Slide>               
+                    </Slider>
+                    <ButtonBack className='back-button'><i class="fa fa-arrow-circle-left" aria-hidden="true"></i></ButtonBack>
+                    <ButtonNext className='next-button'><i class="fa fa-arrow-circle-right" aria-hidden="true"></i></ButtonNext>
+                    <DotGroup className='dot-group' disableActiveDots={false} />
+                </div>
+                
            </CarouselProvider>
             
         </div>
